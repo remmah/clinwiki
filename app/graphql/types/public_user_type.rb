faf06969b602ed8d60a04d553683c998636fa673
Tony Wade <wadant3@gmail.com>
2020-05-05 09:51:26 -0400

    Allow picture url to benull

--- app/graphql/types/public_user_type.rb
+++ app/graphql/types/public_user_type.rb
@@ -4,7 +4,7 @@ module Types
     field :first_name, String, "First name", null: true
     field :last_name, String, "Last name", null: true
     field :review_count, Integer, "Number of reviews the user has done", null: false
-    field :picture_url, String, null: false
+    field :picture_url, String, null: true
 
   end
 end
