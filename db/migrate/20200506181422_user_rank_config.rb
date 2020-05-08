dbbf3a667abc5f68f37deada2f720533fe3fc808
Tony Wade <wadant3@gmail.com>
2020-05-08 11:15:50 -0400

    default json string for ranking

--- db/migrate/20200506181422_user_rank_config.rb
+++ db/migrate/20200506181422_user_rank_config.rb
@@ -1,11 +1,11 @@
 class UserRankConfig < ActiveRecord::Migration[5.2]
   def change
     add_column :sites, :user_rank, :text, default: "#{{
-    default:{gte:0, lte:25},
-    bronze:{gte:26, lte:50},
-    silver:{gte:51, lte:75},
-    gold:{gte:75, lte:100},
-    platinum:{gte:101, lte:10000000},
+    default:{gte:0},
+    bronze:{gte:26},
+    silver:{gte:51},
+    gold:{gte:75},
+    platinum:{gte:101},
   }.to_json} "
   end
 end
